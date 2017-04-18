83d39de7699aea942bab678f2bd023b6b87c7f73
Joris De Winne <joris.dewinne@gmail.com>
2017-04-17 20:29:12 -0700

    Fixing tests

--- src/test/jython/test_get_task.py
+++ src/test/jython/test_get_task.py
@@ -9,23 +9,23 @@ from sdelements.SDEClient import SDEClient
 class TestGetTask(object):
 
     def test_get_task_basic_auth(self, m):
-        m.register_uri('GET', SDEClient.GET_TASK_URI % (self.sde_client.url, '1', '1-T2'), json=GET_TASK_RESPONSE)
         sde_client = SDEClient("http://localhost/sde", "Basic", "admin", "admin", None)
+        m.register_uri('GET', SDEClient.GET_TASK_URI % (sde_client.url, '1', '1-T2'), json=GET_TASK_RESPONSE)
         assert GET_TASK_RESPONSE == sde_client.get_task('1', '1-T2')
 
     def test_get_task_token_auth(self, m):
-        m.register_uri('GET', SDEClient.GET_TASK_URI % (self.sde_client.url, '1', '1-T2'), json=GET_TASK_RESPONSE)
         sde_client = SDEClient("http://localhost/sde", "Token", None, None, "1234abcd")
+        m.register_uri('GET', SDEClient.GET_TASK_URI % (sde_client.url, '1', '1-T2'), json=GET_TASK_RESPONSE)
         assert GET_TASK_RESPONSE == sde_client.get_task('1', '1-T2')
 
     @raises(Exception)
     def test_get_unknown_task(self, m):
-        m.register_uri('GET', SDEClient.GET_TASK_URI % (self.sde_client.url, '1', 'FAILED'), status_Code=403)
         sde_client = SDEClient("http://localhost/sde", "Basic", "admin", "admin", None)
+        m.register_uri('GET', SDEClient.GET_TASK_URI % (sde_client.url, '1', 'FAILED'), status_Code=403)
         sde_client.get_task('1', 'FAILED')
 
     @raises(Exception)
     def test_get_unknown_authentication_method(self, m):
-        m.register_uri('GET', SDEClient.GET_TASK_URI % (self.sde_client.url, '1', '1-T2'), json=GET_TASK_RESPONSE)
         sde_client = SDEClient("http://localhost/sde", "Unknown", None, None, None)
+        m.register_uri('GET', SDEClient.GET_TASK_URI % (sde_client.url, '1', '1-T2'), json=GET_TASK_RESPONSE)
         sde_client.get_task('1', 'FAILED')
